53fe726382b0dc3de49f7197f44ffcc6b4ca7082
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-05-09 15:38:12 +0100

    add logo to sanity studio

---
+++ components/Logo.tsx
@@ -0,0 +1,18 @@
+
+function Logo(props: any) {
+    const {renderDefault, title} = props;
+  return (
+    <div className="flex item-center space-x-2">
+        <img 
+          className="rounded-full object-cover"
+          height={50}
+          width={50}
+          src="https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/63d3b65287589367f19fc287_Screen%20Shot%202023-01-27%20at%201.25.19%20AM.png"
+          alt="logo" 
+        />
+        {renderDefault(props)}
+    </div>
+  )
+}
+
+export default Logo
--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import { myTheme } from './theme'
 import StudioNavbar from './components/StudioNavbar'
+import Logo from './components/Logo'
 
 
 
@@ -23,7 +24,7 @@ export default defineConfig({
 
   studio: {
     components:{
-      // logo: Logo,
+      logo: Logo,
       navbar: StudioNavbar
     }
   },
