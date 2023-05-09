2190474b54e30c77be3ab0ee5642d05323f1a3dc
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-05-09 14:59:06 +0100

    first commit

---
+++ sanity.config.ts
@@ -0,0 +1,31 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+import { myTheme } from './theme'
+import StudioNavbar from './components/StudioNavbar'
+
+
+
+export default defineConfig({
+  basePath:"/studio",
+  name: 'My_Daily_blog_Studio',
+  title: 'Daily Blog Studio',
+
+  projectId: 'on2yqnh5',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+
+  studio: {
+    components:{
+      // logo: Logo,
+      navbar: StudioNavbar
+    }
+  },
+  theme: myTheme
+})
