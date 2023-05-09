9ebdbdad83a0e30b0a6f1d85f2cd9e23598db4f5
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-05-09 15:16:17 +0100

    add costom header on sanity studio

--- components/StudioNavbar.tsx
+++ components/StudioNavbar.tsx
@@ -9,6 +9,17 @@ function StudioNavbar(props: any) {
            <ArrowUturnLeftIcon className="h-6 w-6 text-[#F7AB0A] mr-2" />
            Go To Website
            </Link>
+
+           <div className="hidden md:flex p-5 rounded-lg justify-center border-2 border-[#F7AB0A]">
+            <h1 className="font-bold text-white">
+              Do you want an email to be sent to you about my new websites project?
+            </h1>
+            <Link
+            href="/" 
+            className="text-[#F7AB0A] font-bold ml:2">
+              www.codedmind.com
+            </Link>
+           </div>
       </div>
       <>
         {props.renderDefault(props)}
